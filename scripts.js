042d65077a81f6388f302deac20396f26b9dad80
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-03-14 14:02:16 +0100

    Ejercicios 1 + 2

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 console.log(document.title);
+
+// Ejercicio 1 - Cambia el título "Generation 1 Pokémon" por "Generasión 1 Pokimon".
+const titulo = document.getElementById("gen-1");
+titulo.innerHTML = "Generasión 1 Pokimon";
+
+// Ejercicio 2 - Cambia el color de fondo de la primera generación de Pokimon.
+const fondoGeneracionUno = document.getElementsByClassName(
+  "infocard-list infocard-list-pkmn-lg"
+);
+
+fondoGeneracionUno[0].style.backgroundColor = "yellow";
